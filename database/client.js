a9b89f1ab81464721a7ed0d53dc286475643de7c
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-11-16 19:59:47 -0500

    :sparkles: Update + Delete Books
    Operaciones de actualización, eliminación y consulta de cantidad de páginas agregadas

--- database/client.js
+++ database/client.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@wooks.swarj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
@@ -22,7 +22,7 @@ class MongoDB {
 
                         console.log("BASE DE DATOS CONECTADA");
 
-                        resolve(this.client.db(this.dbname));
+                        resolve(this.client.db(this.dbname).collection('books'));
                     })
                 }
             )
@@ -33,7 +33,64 @@ class MongoDB {
 
     getAll() {
         return this.connect().then((db) => {
-            return db.collection('books').find().toArray();
+            return db.find().toArray();
+        })
+    }
+
+    getOne(id) {
+        return this.connect().then((db) => {
+            return db.find({"_id": ObjectId(id)}).toArray();
+        })
+    }
+
+    getCantidadPaginas (id) {
+        return this.connect().then((db) => {
+            return db.find({"_id": ObjectId(id)}).project({"_id": 0, "paginas": 1}).toArray();
+        })
+    }
+
+    create(newBook) {
+        return this.connect().then((db) => {
+            return db.insertOne(newBook);
+        })
+    }
+
+    updateStatus(id, payload) {
+        
+        return this.connect().then((db) => {
+            if (payload.NewState === "Terminado") {
+                return db.updateOne({"_id": ObjectId(id)}, 
+                    {"$set": {"state": payload.NewState, "review": parseInt(payload.review)},
+                    "$unset": {"actual": "", "progress": ""}});
+            } else if (payload.NewState === "Leyendo") {
+                return db.updateOne({"_id": ObjectId(id)}, 
+                    {"$set": {"state": payload.NewState, "actual": 1, "progress": 0}});
+            }
+        })
+    }
+
+    updateProgress(id, newActual, totalPages){
+        
+        if (totalPages === 0) {
+            return false;
+        } else {
+
+            let newProgress = parseInt(newActual/totalPages*100);
+
+            return this.connect().then((db) => {
+                return db.updateOne({"_id": ObjectId(id)}, {
+                    "$set": {
+                        "actual": newActual,
+                        "progress": newProgress
+                    }
+                })
+            })
+        }
+    }
+
+    delete(id) {
+        return this.connect().then((db) => {
+            return db.deleteOne({"_id": ObjectId(id)});
         })
     }
 };
--- routes/router.js
+++ routes/router.js
@@ -12,28 +12,61 @@ router.get('/', async (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
-    res.send("UN LIBRO EN ESPECIFICO");
+router.get('/:id', async (req, res) => {
+    const book = await client.getOne(req.params.id)
+    res.json({
+        message: 'LIBRO EN ESPECIFICO',
+        data: book
+    })
 })
 
-router.post('/new', (req, res) => {
-    res.send("CREAR LIBRO");
+router.post('/new', async (req, res) => {
+    const book = await client.create(req.body);
+    
+    res.json({
+        message: 'LIBRO CREADO CON ÉXITO',
+        data: book
+    })
 })
 
 router.patch('/:id', (req, res) => {
     res.send("ACTUALIZAR LIBRO");
 })
 
-router.patch('/:id/progress', (req, res) => {
-    res.send("ACTUALIZAR PROGRESO LIBRO");
+router.patch('/:id/progress', async (req, res) => {
+    const TotalPaginas = await client.getCantidadPaginas(req.params.id);
+    const newBookProgress = await client.updateProgress(req.params.id, req.body.newActual, TotalPaginas[0].paginas);
+
+    if (newBookProgress) {
+        
+        res.json({
+            message: 'PROGRESO ACTUALIZADO CON ÉXITO',
+            data: newBookProgress
+        })
+
+    } else {
+        res.send("ESTE LIBRO NO TIENE EL ESTADO LEYENDO");
+    }
 })
 
-router.patch('/:id/status', (req, res) => {
-    res.send("ACTUALIZAR ESTADO LIBRO");
+router.patch('/:id/status', async (req, res) => {
+
+    const updatedStatusBook = await client.updateStatus(req.params.id, req.body)
+
+    res.json({
+        message: 'ESTADO ACTUALIZADO CON ÉXITO',
+        data: updatedStatusBook
+    })
 })
 
-router.delete('/:id', (req, res) => {
-    res.send("ELIMINAR LIBRO");
+router.delete('/:id', async (req, res) => {
+    
+    const deleted = await client.delete(req.params.id);
+
+    res.json({
+        message: 'LIBRO ELIMINADO CON ÉXITO',
+        data: deleted
+    })
 })
 
 module.exports = router;
